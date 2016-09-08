f90cbecd6df6b2e0bde115c89f7d135aa1431b44
laurakathleen <laurakwalley@gmail.com>
2016-09-08 13:29:13 -0700

    sets up app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,19 @@
 /* CLIENT-SIDE JS*/
 
+
+/* hard-coded data! */
+var sampleReviews = [{
+	username: 'Laura',
+	userReview: 'Awesome place!'
+	},
+	{
+	username: 'Abby',
+	userReview: 'Cool drinks!'
+	}];
+
+/* end of hard-coded data */
+
+
 $(document).ready(function() {
 	console.log('app.js loaded!');
 
