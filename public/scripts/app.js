fb2d3679971fefd3345d9099049970e7139176e7
laurakathleen <laurakwalley@gmail.com>
2016-09-08 17:59:44 -0700

    works on update/put

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,13 +5,37 @@ var allReviews = [];
 $(document).ready(function() {
 	console.log('app.js loaded!');
 
-
+	//render all reviews
 	$.ajax({
 		method: 'GET',
 		url: '/api/reviews',
 		success: onSuccess,
 		error: onError
 	})
+
+	//update review
+
+	$('#one-review').on('click', '#updateBtn', function(){
+		$(this).parent().find('.collapse').removeClass('collapse');
+		console.log('update btn clicked');
+	// $('updateBtn').on('click', function(){
+	// 	console.log('update btn clicked');
+	// 	$('.collapse').slideToggle('slow');
+	// });
+	// $('#one-review').on('click', '#updateBtn', function(event){
+	// 	event.preventDefault();
+	// 	$('.collapse').toggle('visibility', 'visible');
+		//find closest id:
+		//var reviewId = $('#one-review').closest('.collapse').attr('data-id');
+	});
+	var updateReview = $(this).serialize();
+	$.ajax({
+		method: 'PUT',
+		url: '/api/reviews/:id',
+		data: updatedReview,
+		success: onUpdateSuccess,
+		error: onError
+	});
 
 });
 
@@ -29,6 +53,12 @@ function renderReviews(reviews) {
 	console.log(reviewHtml); 
 }
 
+function onUpdateSuccess(updatedReview) {
+	// var reviewId = updatedReview._id;
+	// $('[data-id=' + reviewId + ']').remove();
+	// renderReviews(updatedReview);
+	console.log(updatedReview);
+}
 
 
 function onError(error) {
